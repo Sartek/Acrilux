546096e40aa6d001bd29c0175392b8421800af0a
Sartek <sartek.richards@gmail.com>
2012-05-28 23:32:35 +1000

    Fixed bug to do with polling events in 2 places

--- Main.cpp
+++ Main.cpp
@@ -19,12 +19,6 @@ int main()
 
     while(Window.isOpen())
     {
-        sf::Event event;
-        while (Window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                Window.close();
-        }
 
         switch (State)
         {
